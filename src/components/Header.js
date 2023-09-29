736b2698684bf607855eafd1bf1b9346e9b806a6
artem alexeenko <artyom.alexeyenko@gmail.com>
2023-09-29 23:24:19 +0600

    feat: add contexts, components in project. header, product, sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+// sibebar context
+import { SidebarContext } from '../contexts/SidebarContext';
+// import icons
+import { BsBag } from 'react-icons/bs';
 
 const Header = () => {
-  return <div>Header</div>;
+  const {isOpen, setIsOpen} = useContext(SidebarContext);
+  return <div>
+    <header className='bg-pink-200'>Header
+      <div onClick={() => setIsOpen(!isOpen)} className='cursor-pointer flex relative'>
+        <BsBag className='text-2xl' />
+      </div>
+    </header>
+  </div>;
 };
 
 export default Header;
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {createContext, useState, useEffect} from 'react';
 
-const CartContext = () => {
-  return <div>CartContext</div>;
+// create context
+export const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+  // cart state
+  const [cart, setCart] = useState([]);
+  // add to cart
+  const addToCart = () => {
+    console.log('added to the cart');
+  };
+  return <CartContext.Provider value={ addToCart }>
+    {children}
+  </CartContext.Provider>;
 };
 
-export default CartContext;
+export default CartProvider;
