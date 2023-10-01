9f0ae3937fb12fbefbd97de7d67077013def37f5
artem alexeenko <artyom.alexeyenko@gmail.com>
2023-10-01 16:18:44 +0600

    feat: add cartItem, cartContext, edit product, sidebar components

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -7,10 +7,27 @@ const CartProvider = ({ children }) => {
   // cart state
   const [cart, setCart] = useState([]);
   // add to cart
-  const addToCart = () => {
-    console.log('added to the cart');
+  const addToCart = (product, id) => {
+    const newItem = {...product, amount: 1};
+    // check if the item is already in the cart
+    const cartItem = cart.find(item => {
+      return item.id === id;
+    })
+    // if cart item is already in the cart
+    if (cartItem) {
+      const newCart = [...cart].map(item => {
+        if (item.id === id) {
+          return {...item, amount: cartItem.amount + 1};
+        } else {
+          return item;
+        }
+      })
+      setCart(newCart);
+    } else {
+      setCart([...cart, newItem]);
+    }
   };
-  return <CartContext.Provider value={ addToCart }>
+  return <CartContext.Provider value={ {cart, addToCart} }>
     {children}
   </CartContext.Provider>;
 };
